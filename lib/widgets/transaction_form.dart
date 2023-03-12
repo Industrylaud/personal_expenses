a6cfa942ecc0fc8289b1996297398afcb95c03e0
Industrylaud <mrfros22@gmail.com>
2023-03-12 16:56:50 +0100

    Add Date picker

--- lib/widgets/transaction_form.dart
+++ lib/widgets/transaction_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TransactionForm extends StatefulWidget {
   final Function submitHandler;
@@ -10,12 +11,13 @@ class TransactionForm extends StatefulWidget {
 }
 
 class _TransactionFormState extends State<TransactionForm> {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime _selectedDate;
 
   void _submitData() {
-    final enteredTitle = titleController.text;
-    final enteredAmount = double.parse(amountController.text);
+    final enteredTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
 
     if (enteredTitle.isEmpty || enteredAmount <= 0) {
       return;
@@ -24,6 +26,23 @@ class _TransactionFormState extends State<TransactionForm> {
     widget.submitHandler(enteredTitle, enteredAmount);
 
     Navigator.of(context).pop();
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(2019),
+            lastDate: DateTime.now())
+        .then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
   }
 
   @override
@@ -38,22 +57,28 @@ class _TransactionFormState extends State<TransactionForm> {
             children: [
               TextField(
                 decoration: InputDecoration(labelText: 'Title'),
-                controller: titleController,
+                controller: _titleController,
                 onSubmitted: (_) => _submitData(),
               ),
               TextField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(labelText: 'Amount'),
-                controller: amountController,
+                controller: _amountController,
                 onSubmitted: (_) => _submitData(),
               ),
               Container(
                 height: 70,
                 child: Row(
                   children: [
-                    Text('No date chosen'),
+                    Expanded(
+                      child: Text(
+                        _selectedDate == null
+                            ? 'No date chosen'
+                            : 'Picked date: ${DateFormat.yMMMd().format(_selectedDate)}',
+                      ),
+                    ),
                     TextButton(
-                      onPressed: () {},
+                      onPressed: _presentDatePicker,
                       child: Text(
                         'Choose date',
                         style: TextStyle(fontWeight: FontWeight.bold),
